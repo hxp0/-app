9b5493c54881a345309a4d45fdce1f64253bbb28
胡小鹏 <2721118433@qq.com>
2024-12-17 22:55:34 +0800

    search

--- uni-music/src/pages/mine/mine.vue
+++ uni-music/src/pages/mine/mine.vue
@@ -1,11 +1,51 @@
 <template>
-    
+<view class="content">
+    <view class="header">
+        <uni-icons type="bars" size="20" color="#fff"></uni-icons>
+        <view>
+            <uni-icons type="search" size="20" color="#fff" class="search" @click="goSearch"></uni-icons>
+            <uni-icons type="more-filled" size="20" color="#fff"></uni-icons>
+        </view>
+    </view>
+    <view class="top"></view>
+</view> 
 </template>
 
 <script setup lang="ts">
-
+const goSearch = ()=>{
+    uni.navigateTo({
+        url: '/pages/search/Search'
+    })
+}
 </script>
 
 <style lang="scss" scoped>
+.content{
+    width: 100%;
+    height: 100%;
+    background-color: #f5f5f5;
+    position: relative;
+}
+::v-deep .header{
+    width: 100%;
+    height: 40px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    view{
+        padding: 10px;
+        .search{
+        margin-right: 10px;
+       }
 
+    }
+}
+.top{
+    width: 100%;
+    height: 250px;
+    background-color: #4e1414;
+}
 </style>
