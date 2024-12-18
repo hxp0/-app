2c8cfb47db4f7c13bf233ce79bdb746f5c32fb92
赵城彬 <1697750360@qq.com>
2024-12-18 16:39:28 +0800

    增

--- uni-music/src/services/index.ts
+++ uni-music/src/services/index.ts
@@ -1,14 +1,8 @@
 
-<<<<<<< HEAD
 interface RequestParams {
     url: string
     data?: object
   }
-=======
-export interface RequestParams {
-    url:string,
-    data?:object
-}
 export interface RequestList {
     targetId:number
     pic:string
@@ -18,8 +12,6 @@ export interface RequestRes {
     code:number
     data:RequestList[]
 }
-
-
 
 
 
@@ -80,7 +72,6 @@ export const getDetailApi = (id:number) => {
 
 
 
->>>>>>> zcb
   // 封装请求接口的函数，统一管理接口
   export const request = <T>({ url, data }: RequestParams) => {
     return new Promise<T>((resolve, reject) => {
@@ -168,85 +159,17 @@ export const CaptchaApi = (phone:number) => {
     }
    })
 }
+
+
+
   
-  
-<<<<<<< HEAD
-export interface RequestParams {
-    url:string,
-    data?:object
-}
-export interface RequestList {
-    targetId:number
-    pic:string
-    typeTitle:string
-}
-export interface RequestRes {
-    code:number
-    data:RequestList[]
-}
 
 
 
 
 
-export enum ShowType {
-    HOMEPAGE_SLIDE_PLAYLIST = 'HOMEPAGE_SLIDE_PLAYLIST',
-    HOMEPAGE_SLIDE_SONGLIST_ALIGN = 'HOMEPAGE_SLIDE_SONGLIST_ALIGN'
-}
-export interface Block {
-    blockCode:string
-    showType:string
-    creatives:{
-        creativeType:string
-        resources:{
-            resourceId:string
-            uiElement:{
-                image:{
-                    imageUrl:string
-                }
-                mainTitle:{
-                    title:string
-                }
-            }
-        }
-    }
-    uiElement:{
-        subTitle:{
-            title:string
-        }
-    }
-}
-export interface RequestHome {
-    code:number
-    data:{
-        blocks:Block[]
-    }
-}
 
 
 
-export const request = <T>({url,data}:RequestParams) => {
-    return new Promise<T>((resolve,reject) => {
-        uni.request({
-            url,
-            data,
-            success:(res) =>{
-                resolve(res.data as T)
-            },
-            fail:reject
-        })
-    })
-}
 
 
-
-export const getBannerApi = () => {
-    return request<RequestRes>({url:'https://zyxcl.xyz/music/api/dj/banner'})
-}
-export const getHomeApi = () => {
-    return request<RequestHome>({url:'https://zyxcl.xyz/music/api/homepage/block/page'})
-}
-
-
-=======
->>>>>>> zcb
