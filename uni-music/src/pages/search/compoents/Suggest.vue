949b9236c35fe9e84ccaa822fe9b0201708288ba
周凯玉 <yu20011217@126.com>
2024-12-18 08:21:42 +0800

    tabBar

---
+++ uni-music/src/pages/search/compoents/Suggest.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import  type { SearchSuggestItem } from '../../../services'
+interface Props {
+    list:SearchSuggestItem[]
+}
+const props = defineProps<Props>()
+
+</script>
+
+<template>
+<view class="suggest">
+    <view v-for="item in props.list" :key="item.keyword" class="item">
+        <uni-icons type="search" size="20" color="#eee" class="search"></uni-icons>
+        {{ item.keyword }}
+    </view>
+</view>
+
+</template>
+
+<style lang="scss" scoped>
+.suggest{
+    width: 100%;
+    .item{
+        width: 100%;
+        height: 40px;
+        line-height: 40px;
+        font-size: 14px;
+        color: #333;
+        border-bottom: 1px solid #eee;
+        padding: 0 10px;
+    }
+}
+
+</style>
