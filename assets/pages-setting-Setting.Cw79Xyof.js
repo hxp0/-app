95321728343e6c7a7d0e31871275953d27def395
周凯玉 <yu20011217@126.com>
2024-12-22 20:50:55 +0800

    Updates

---
+++ assets/pages-setting-Setting.Cw79Xyof.js
@@ -0,0 +1,1 @@
+import{c as e,w as r,i as s,o,m as t}from"./index-DjBnUkLC.js";import{_ as a}from"./plugin-vueexport-helper.BCo6x5W8.js";const n=a({},[["render",function(a,n){const u=s;return o(),e(u,null,{default:r((()=>[t("设置")])),_:1})}]]);export{n as default};
